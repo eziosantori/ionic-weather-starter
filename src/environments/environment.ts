df10c98241bef334ecc32613481a199a1b643222
Ken Sodemann <ken@ionic.io>
2019-06-09 10:04:28 -0500

    step(get-data): create the weather service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  appId: '357d7d7f94bf6a9b04b534db299a1a3b',
+  baseUrl: 'https://api.openweathermap.org/data/2.5',
   production: false
 };
 
