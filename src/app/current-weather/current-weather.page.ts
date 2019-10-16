84760af4d1883ab0657f066372d75317e7fe04c6
Ken Sodemann <ken@ionic.io>
2019-10-16 13:59:12 -0500

    step(ui-mock): mock up the UI

--- src/app/current-weather/current-weather.page.ts
+++ src/app/current-weather/current-weather.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Weather } from '../models/weather';
+import { IconMapService } from '../services/icon-map/icon-map.service';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: 'current-weather.page.html',
   styleUrls: ['current-weather.page.scss']
 })
-export class CurrentWeatherPage {}
+export class CurrentWeatherPage {
+  currentWeather: Weather = {
+    temperature: 302,
+    condition: 200
+  };
+
+  constructor(public iconMap: IconMapService) { }
+}
