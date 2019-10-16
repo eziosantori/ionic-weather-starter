35117ddb265d50bef627b2ac3b261471c60a4bde
Ken Sodemann <ken@ionic.io>
2019-10-16 15:48:07 -0500

    step(use-data): use the data from the sesrvice

--- src/app/forecast/forecast.page.spec.ts
+++ src/app/forecast/forecast.page.spec.ts
@@ -1,8 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
 
 import { ForecastPage } from './forecast.page';
+import { WeatherService } from '../services/weather/weather.service';
+import { createWeatherServiceMock } from '../services/weather/weather.service.mock';
 
 describe('ForecastPage', () => {
   let component: ForecastPage;
@@ -12,6 +16,7 @@ describe('ForecastPage', () => {
     TestBed.configureTestingModule({
       declarations: [ForecastPage],
       imports: [IonicModule.forRoot()],
+      providers: [{ provide: WeatherService, useFactory: createWeatherServiceMock }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
 
@@ -22,5 +27,46 @@ describe('ForecastPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('entering the page', () => {
+    it('gets the forecast', () => {
+      const weather = TestBed.get(WeatherService);
+      component.ionViewDidEnter();
+      expect(weather.forecast).toHaveBeenCalledTimes(1);
+    });
+
+    it('shows the forecast items', () => {
+      const weather = TestBed.get(WeatherService);
+      weather.forecast.and.returnValue(
+        of([
+          [
+            {
+              temperature: 300,
+              condition: 200,
+              date: new Date(2018, 8, 19)
+            }
+          ],
+          [
+            {
+              temperature: 265,
+              condition: 601,
+              date: new Date(2018, 8, 20)
+            }
+          ],
+          [
+            {
+              temperature: 293,
+              condition: 800,
+              date: new Date(2018, 8, 21)
+            }
+          ]
+        ])
+      );
+      component.ionViewDidEnter();
+      fixture.detectChanges();
+      const f = fixture.debugElement.queryAll(By.css('kws-daily-forecast'));
+      expect(f.length).toEqual(3);
+    });
   });
 });
